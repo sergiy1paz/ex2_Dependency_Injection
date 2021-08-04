06ecd7ee3b28280bb062515f281ddf04f40d77ad
Sergiy Pazyuk <pazyk.15@gmail.com>
2021-08-04 23:33:40 +0300

    Добавьте файлы проекта.

---
+++ ex2_Dependency_Injection/Controllers/ArticleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex2_Dependency_Injection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        public ArticleController()
+        {
+
+        }
+
+        public IActionResult PublishArticle()
+        {
+
+
+            return Ok();
+        }
+    }
+}
