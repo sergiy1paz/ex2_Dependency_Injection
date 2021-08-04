d5d34fb37eb9c671ad118320b7fe4095c672fcdc
Sergiy Pazyuk <pazyk.15@gmail.com>
2021-08-05 00:17:26 +0300

    Was created two libraries for aplying services

---
+++ Services/CheckArticleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace Services
+{
+    public class CheckArticleService : ICheckArticleService
+    {
+        public bool Check()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/PublishArticleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace Services
+{
+    public class PublishArticleService : IPublishArticleService
+    {
+        public bool Publish()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
